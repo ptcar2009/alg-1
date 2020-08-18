bed827d96c8dee8845dda50015f0fea475dc64ce
Pedro Tavares <ptcar@inspr.com>
2020-08-17 23:46:43 -0300

    feat: adds benchmark compiling

--- TP1/src/main.cpp
+++ TP1/src/main.cpp
@@ -1,12 +1,20 @@
 #include <queue>
 #include <iostream>
+#include <string>
 #include "game.hpp"
 int main(int argc, char **argv)
 {
     if (argc < 2)
     {
-        throw "not enought arguments";
+        std::cout << "not enough arguments" << std::endl
+                  << "usage: " << argv[0] << " [-b | --benchmark] (file)";
     }
+#if defined(BENCHMARK)
+    clock_t t;
+
+    t = clock();
+
+#endif // BENCHMARK
 
     auto game = new Game(argv[1]);
     auto result = game->Play();
@@ -20,4 +28,10 @@ int main(int argc, char **argv)
         std::cout << result.first << std::endl
                   << result.second << std::endl;
     }
+#if defined(BENCHMARK)
+
+    t = clock() - t;
+    std::cout << "\nnumber of clock cycles: " << t << std::endl;
+
+#endif // BENCHMARK
 }
